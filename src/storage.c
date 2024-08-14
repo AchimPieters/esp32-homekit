7658e55387661a42eb017463abaa9d3680b8ad4c
Achim Pieters <Achim.Pieters@outlook.com>
2024-08-14 20:33:46 +0200

    Updated to use NVS storage
    Updated to use NVS storage instead of a partition.

--- src/storage.c
+++ src/storage.c
@@ -1,3 +1,28 @@
+/**
+
+   Copyright 2024 Achim Pieters | StudioPieters®
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+   for more information visit https://www.studiopieters.nl
+
+ **/
+
 #include <string.h>
 #include <ctype.h>
 #include "constants.h"
@@ -13,7 +38,6 @@
 #define MAX_PAIRINGS 16
 #define PAIRING_KEY_PREFIX "pairing_"
 #define PAIRING_KEY_MAX_LEN (sizeof(PAIRING_KEY_PREFIX) + 10 + 1) // 10 for maximum int32 + 1 for null terminator
-#define ACCESSORY_ID_SIZE 36
 #define ACCESSORY_KEY_SIZE 64
 
 static nvs_handle_t homekit_nvs_handle;

e0d51473973448915de0426de5c86d32418859aa
Achim Pieters <info@studiopieters.nl>
2026-02-20 14:50:55 +0100

    Dist 2.0.2
    Dist 2.0.2

---
+++ dist/esp32-homekit_2.0.2/src/base64.c
@@ -0,0 +1,114 @@
+/**
+
+   Copyright 2026 Achim Pieters | StudioPieters®
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+   for more information visit https://www.studiopieters.nl
+
+ **/
+
+#include "base64.h"
+
+static unsigned char base64_chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+unsigned char base64_encode_char(unsigned char c) {
+        return base64_chars[c];
+}
+
+unsigned char base64_decode_char(unsigned char c) {
+        if (c >= 'A' && c <= 'Z')
+                return c - 'A';
+        if (c >= 'a' && c <= 'z')
+                return c - 'a' + 26;
+        if (c >= '0' && c <= '9')
+                return c - '0' + 52;
+        if (c == '+')
+                return 62;
+        if (c == '/')
+                return 63;
+        if (c == '=')
+                return 64;
+
+        return 0;
+}
+
+size_t base64_encoded_size(const unsigned char *data, size_t size) {
+        return (size + 2)/3*4;
+}
+
+size_t base64_decoded_size(const unsigned char *encoded_data, size_t encoded_size) {
+        if (encoded_size == 0)
+                return 0;
+
+        size_t size = (encoded_size + 3) / 4 * 3;
+        if (encoded_size > 0 && encoded_data[encoded_size - 1] == '=')
+                size--;
+        if (encoded_size > 1 && encoded_data[encoded_size - 2] == '=')
+                size--;
+        return size;
+}
+
+int base64_encode(const unsigned char* data, size_t size, unsigned char *encoded_data) {
+        size_t i=0, j=0, size1=size - size%3;
+        for (; i<size1; i+=3, j+=4) {
+                encoded_data[j+0] = base64_encode_char(data[i+0]>>2);
+                encoded_data[j+1] = base64_encode_char(((data[i+0]&0x3)<<4) + (data[i+1]>>4));
+                encoded_data[j+2] = base64_encode_char(((data[i+1]&0xF)<<2) + (data[i+2]>>6));
+                encoded_data[j+3] = base64_encode_char(data[i+2]&0x3F);
+        }
+
+        if (size % 3 == 1) {
+                encoded_data[j+0] = base64_encode_char(data[i+0]>>2);
+                encoded_data[j+1] = base64_encode_char((data[i+0]&0x3)<<4);
+                encoded_data[j+2] = encoded_data[j+3] = '=';
+                j += 4;
+        } else if (size % 3 == 2) {
+                encoded_data[j+0] = base64_encode_char(data[i+0]>>2);
+                encoded_data[j+1] = base64_encode_char(((data[i+0]&0x3)<<4) + (data[i+1]>>4));
+                encoded_data[j+2] = base64_encode_char(((data[i+1]&0xF)<<2));
+                encoded_data[j+3] = '=';
+                j += 4;
+        }
+
+        return j;
+}
+
+int base64_decode(const unsigned char* encoded_data, size_t encoded_size, unsigned char *data) {
+        if (encoded_size % 4 != 0)
+                return -1;
+
+        size_t i=0, j=0;
+        for (; i<encoded_size; i+=4) {
+                unsigned char block[4];
+                for (size_t k=0; k<4; k++)
+                        block[k] = base64_decode_char(encoded_data[i+k]);
+
+                data[j++] = (block[0]<<2) + (block[1]>>4);
+                if (block[2] == 64)
+                        break;
+
+                data[j++] = ((block[1]&0xF)<<4) + (block[2]>>2);
+                if (block[3] == 64)
+                        break;
+
+                data[j++] = ((block[2]&0x3)<<6) + block[3];
+        }
+
+        return j;
+}
